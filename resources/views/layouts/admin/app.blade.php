162f057f3a67c992aa7a3b74a7c7b7ebd34db4b1
jundialfaruqi <jundialfaruqi@gmail.com>
2026-04-25 03:29:40 +0700

    feat: reposition theme toggle button and update notification dropdown alignment in admin layout

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -101,6 +101,23 @@
                 </div>
 
                 <div class="flex items-center gap-3">
+
+                    <!-- Theme Toggle -->
+                    <button id="theme-toggle" class="btn btn-circle btn-primary hover:bg-base hover:text-white">
+                        <!-- Sun Icon -->
+                        <svg id="sun-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+                        </svg>
+                        <!-- Moon Icon -->
+                        <svg id="moon-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5 hidden">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                        </svg>
+                    </button>
+
                     <!-- Section Notifications -->
                     @php
                         $user = auth()->user();
@@ -135,7 +152,7 @@
                                 ];
                             });
                     @endphp
-                    <div class="dropdown dropdown-center">
+                    <div class="dropdown dropdown-end">
                         <button tabindex="0" class="btn btn-secondary btn-circle">
                             <div class="indicator">
                                 @if ($dashboardNotifications->count() > 0)
@@ -224,21 +241,7 @@
                             </div>
                         </div>
                     </div>
-                    <!-- Theme Toggle -->
-                    <button id="theme-toggle" class="btn btn-circle btn-primary hover:bg-base hover:text-white">
-                        <!-- Sun Icon -->
-                        <svg id="sun-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-                        </svg>
-                        <!-- Moon Icon -->
-                        <svg id="moon-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5 hidden">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-                        </svg>
-                    </button>
+
 
                     <!-- Divider -->
                     <div class="hidden md:block h-8 border-l border-dotted border-base-content/20"></div>
