75aa4d3b5b0b5fface1652a46e490246fdd91119
jundialfaruqi <jundialfaruqi@gmail.com>
2026-04-27 00:11:54 +0700

    feat: add mobile_auth middleware to enforce API key validation for all API routes

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AttendanceController;
 
-Route::get('/personnels', [AttendanceController::class, 'personnels']);
-Route::get('/personnels/check-status/{id}', [AttendanceController::class, 'checkStatus']);
-Route::post('/login/pin', [AttendanceController::class, 'login']);
+Route::middleware('mobile_auth')->group(function () {
+    Route::get('/personnels', [AttendanceController::class, 'personnels']);
+    Route::get('/personnels/check-status/{id}', [AttendanceController::class, 'checkStatus']);
+    Route::post('/login/pin', [AttendanceController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/absensi', [AttendanceController::class, 'store']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/absensi', [AttendanceController::class, 'store']);
+    });
 });
