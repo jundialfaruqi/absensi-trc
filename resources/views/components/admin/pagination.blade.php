c45b848b14aa37fc8eecbf3e12b7580b8b75f437
jundialfaruqi <jundialfaruqi@gmail.com>
2026-05-14 02:32:38 +0700

    feat: implement responsive mobile pagination view with simplified navigation controls

--- resources/views/components/admin/pagination.blade.php
+++ resources/views/components/admin/pagination.blade.php
@@ -12,7 +12,39 @@
         </div>
 
         {{-- Pagination Links --}}
-        <div class="join">
+        
+        {{-- Mobile View: Ringkas (Hanya Prev, Current/Total, Next) --}}
+        <div class="flex md:hidden join">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <button class="join-item btn btn-sm btn-disabled" disabled aria-disabled="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" /></svg>
+                </button>
+            @else
+                <button class="join-item btn btn-sm" wire:click="previousPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" rel="prev">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" /></svg>
+                </button>
+            @endif
+
+            {{-- Current Page / Total Page --}}
+            <button class="join-item btn btn-sm btn-disabled text-base-content font-bold" disabled>
+                {{ $paginator->currentPage() }} / {{ $paginator->lastPage() }}
+            </button>
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <button class="join-item btn btn-sm" wire:click="nextPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" rel="next">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" /></svg>
+                </button>
+            @else
+                <button class="join-item btn btn-sm btn-disabled" disabled aria-disabled="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" /></svg>
+                </button>
+            @endif
+        </div>
+
+        {{-- Desktop View: Deretan Angka dengan Auto-Cut dari Laravel --}}
+        <div class="hidden md:flex join">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
                 <button class="join-item btn btn-sm btn-disabled" disabled aria-disabled="true">
