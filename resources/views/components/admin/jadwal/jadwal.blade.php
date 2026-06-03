7e6e51fa474a449edb275ab5371ccf5d40fbee91
jundialfaruqi <jundialfaruqi@gmail.com>
2026-06-04 01:50:44 +0700

    refactor: remove redundant loading states from jadwal cell interactions

--- resources/views/components/admin/jadwal/jadwal.blade.php
+++ resources/views/components/admin/jadwal/jadwal.blade.php
@@ -284,19 +284,9 @@
                                         <td wire:key="cell-{{ $p->id }}-{{ $date }}"
                                             class="text-center border-r border-base-200 p-0 h-14 cursor-pointer hover:opacity-80 transition-all relative {{ $isToday && !$j ? 'bg-primary/10' : '' }} {{ $cellClass }} {{ $j && $j->is_manual ? 'bg-pattern-manual' : '' }}"
                                             style="{{ $style }}"
-                                            wire:click="openQuickAdd('{{ $p->id }}', '{{ $date }}', '{{ $p->attendance_type }}')"
-                                            wire:loading.class="opacity-40 pointer-events-none"
-                                            wire:target="openQuickAdd('{{ $p->id }}', '{{ $date }}', '{{ $p->attendance_type }}')">
+                                            wire:click="openQuickAdd('{{ $p->id }}', '{{ $date }}', '{{ $p->attendance_type }}')">
 
-                                            <div class="relative w-full h-full flex items-center justify-center">
-                                                {{-- Specific Cell Loader --}}
-                                                <div wire:loading
-                                                    wire:target="openQuickAdd('{{ $p->id }}', '{{ $date }}', '{{ $p->attendance_type }}')"
-                                                    class="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 flex items-center justify-center z-20">
-                                                    <span
-                                                        class="loading loading-spinner loading-xs text-primary"></span>
-                                                </div>
-
+                                            <div class="w-full h-full flex items-center justify-center">
                                                 @if ($j)
                                                     <div
                                                         class="flex flex-col items-center justify-center w-full h-full relative font-bold">
