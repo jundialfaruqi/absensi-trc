aace023c4f386945dd94fb648bfc96e1402f1c58
jundialfaruqi <jundialfaruqi@gmail.com>
2026-09-19 03:09:56 +0700

    refactor: update styling for personal device status in personnel edit component

--- resources/views/components/admin/personnel-edit/personnel-edit.blade.php
+++ resources/views/components/admin/personnel-edit/personnel-edit.blade.php
@@ -341,18 +341,16 @@
                                 <div class="form-control w-full md:col-span-2">
                                     @if ($has_personal_device)
                                         <div
-                                            class="flex items-center gap-4 bg-success/5 p-4 rounded-xl border border-success/20">
-                                            <div class="bg-success/10 p-2 rounded-lg">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                                                    class="w-5 h-5 text-success">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                </svg>
-                                            </div>
+                                            class="flex items-center gap-4 bg-base-200/50 p-4 rounded-xl border border-base-300">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                class="w-6 h-6 text-success shrink-0">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                            </svg>
                                             <div class="flex-1 min-w-0">
                                                 <span
-                                                    class="label-text font-bold block uppercase text-xs text-success">Perangkat
+                                                    class="label-text font-bold block uppercase text-xs whitespace-normal text-base-content/70">Perangkat
                                                     Terdaftar</span>
                                                 <span
                                                     class="text-[10px] text-base-content opacity-60 block truncate italic">
